d22671c38c4479c01d343c54cdf57e1764238c8c
Raul Oteiza <raulote147@gmail.com>
2024-02-27 21:04:35 +0100

    Primer commit

---
+++ src/main/java/com/mycompany/prgpruebagit/PrgPruebaGit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prgpruebagit;
+
+/**
+ *
+ * @author Vespertino
+ */
+public class PrgPruebaGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
